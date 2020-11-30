3b765774d378cf8f800259f5d841751496288189
Luke Forehand <lforehan@amfam.com>
2020-11-30 17:28:58 -0600

    changed .data and models path

--- deertracker/__init__.py
+++ deertracker/__init__.py
@@ -1,15 +1,11 @@
 import pathlib
 
-DEFAULT_DATA_STORE = pathlib.Path(__file__).parent.absolute() / ".data"
+DEFAULT_DATA_STORE = pathlib.Path(".data")
 DEFAULT_DATA_STORE.mkdir(exist_ok=True)
 
 DEFAULT_PHOTO_STORE = pathlib.Path(DEFAULT_DATA_STORE / "photos")
 DEFAULT_PHOTO_STORE.mkdir(exist_ok=True)
 
-RELATIVE_PHOTO_STORE = DEFAULT_PHOTO_STORE.relative_to(DEFAULT_DATA_STORE)
-
 DEFAULT_DATABASE = DEFAULT_DATA_STORE / "deertracker.db"
 
-DEFAULT_MODEL_PATH = str(
-    pathlib.Path(__file__).parent.absolute() / "models/md_v4.1.0.pb"
-)
+DEFAULT_MODEL_PATH = "models/md_v4.1.0.pb"
